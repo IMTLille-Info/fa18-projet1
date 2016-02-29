503da53d0740e44ecaf8d3999ef4eb44a263f438
BenjaminGuilbert <guilbertbenjamin@live.fr>
2016-02-29 11:08:26 +0100

    create maven project

---
+++ game/src/main/java/telecom/fa18/projet1/game/App.java
@@ -0,0 +1,13 @@
+package telecom.fa18.projet1.game;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
