4cac1adb20be754d32b9cafc5cb208528e18582e
BenjaminGuilbert <guilbertbenjamin@live.fr>
2016-03-02 16:50:48 +0100

    hero's moving

---
+++ core/src/main/java/projet1/core/InputKeyboard.java
@@ -0,0 +1,76 @@
+package projet1.core;
+
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputProcessor;
+
+public class InputKeyboard implements InputProcessor {
+	
+	private Hero hero;
+	
+	public InputKeyboard(Hero hero){
+		this.hero = hero;
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		switch(keycode){
+		case Keys.UP : 
+			this.hero.moveforward();
+			return true;
+		case Keys.DOWN : 
+			this.hero.moveBack();
+			return true;
+		case Keys.LEFT : 
+			this.hero.moveLeft();
+			return true;
+		case Keys.RIGHT : 
+			this.hero.moveRight();
+			return true;
+		default:
+			return false;
+		}		
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
